83d73d7880a7d9ce9e7ed0144ea3ce9e272a178d
SangsunKim0 <dy20627370@gmail.com>
2026-06-04 15:42:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,23 @@ function validate(e) { // 전송 제어 (onsubmit)
     e.preventDefault(); // 페이지 이동 막기 공통()
     return false;
 }
+
+// 1. 초기 좌표 설정
+var x = 100, y = 100;
+// 2. 페이지 로딩이 완료되면(onload) 실행
+window.onload = function () {
+    // 3. 키보드를 누를 때마다(onkeydown) 실행
+    window.onkeydown = function (e) {
+        // 37:Left , 38:Up, 39:Right , 40:Down
+        switch (e.keyCode) {
+            case 37: x -= 10; break; // 왼쪽
+            case 38: y -= 10; break; // 위쪽
+            case 39: x += 10; break; // 오른쪽
+            case 40: y += 10; break; // 아래쪽
+        }
+        // 변경된 좌표를 자동차(DOM)에 반영
+        var car = document.getElementById("car");
+        car.style.left = x + "px";
+        car.style.top = y + "px";
+    };
+};
